c746257b45829cfebea88967f21fba7cbfba53f1
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-12-09 07:11:11 +0530

    Update Coin Combinations I.cpp

--- Dynamic Programming/Coin Combinations I.cpp
+++ Dynamic Programming/Coin Combinations I.cpp
@@ -1,53 +1,34 @@
-//Method-1 : Will give TLE for large test cases
-
-
 #include<bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
-using namespace __gnu_pbds;
-#define fast ios_base::sync_with_stdio(0),cin.tie(0)
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define ll long long
-int mod=1e9+7;
-ll int ways(vector<ll int>&vec,vector<ll int>&dp,ll int x)
-{
-    if(x==0)
-    {
-        return 1;//Sucesufully Distributed
-    }
-    if(dp[x]!=-1)
-    {
-        return dp[x];
-    }
-    ll int ans=0;
-    for(int i=0;i<vec.size();i++)
-    {
-        if(vec[i]<=x)
-        {
-            ans+=ways(vec,dp,x-vec[i])%mod;
-        }
-        else
-        {
-            break;
-        }
-    }
-    return dp[x]=ans;
-}
 int main()
 {
+    int mod=1e9+7;
     fast;
-    ll int n,x;
+    int n,x;
     cin>>n>>x;
-    vector<ll int>vec(n);
-    vector<ll int>dp(x+1,-1);
+    vector<int>vec(n);
     for(int i=0;i<n;i++)
     {
         cin>>vec[i];
     }
-    sort(vec.begin(),vec.end());
-    ll int res=ways(vec,dp,x)%mod;
-   
-    cout<<max(0LL,res)<<endl;
+
+    int dp[x+1];
+    dp[0]=1;
+    for(int i=1;i<=x;i++)
+    {
+        dp[i]=0;
+        for(auto c:vec)
+        {
+            if((i-c)>=0)
+            {
+                dp[i]=(dp[i]+dp[i-c])%mod;
+            }
+            
+        }
+    }
+    cout<<dp[x]<<endl;
     
     return 0;
 }
